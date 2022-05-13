9498647df725e084ea67b30315ceccbab29571c9
JuanDAC <juanavila12.0308@gmail.com>
2022-05-13 16:52:28 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# bootcamp_coderise
+# My_todos_of_today
---
+++ js/storage.js
@@ -0,0 +1,22 @@
+
+const KEY_APP = 'coderise';
+
+export const load = () => {
+    /*
+        variable constante
+    */
+    const key = `${KEY_APP}-tasks`;
+    const valueInString = localStorage.getItem(key) ?? '[]';
+    const value = JSON.parse(valueInString);
+    return value;
+};
+
+
+export const save = (tasks = []) => {
+    const key = `${KEY_APP}-tasks`;
+    if (!Array.isArray(tasks))
+        return null;
+    const valueToString = JSON.stringify(tasks);
+    localStorage.setItem(key, valueToString);
+}
+
